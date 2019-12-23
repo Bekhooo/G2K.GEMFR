1734d34216e10d029ea89ff4f4f82f0742ea903b
Bekhooo <mo7amed.zeyada@gmail.com>
2019-12-23 17:04:45 +0200

    Add project files.

---
+++ G2K.GEMFR.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="G2K.GEMFR.Website.MvcApplication" Language="C#" %>
---
+++ G2K.GEMFR.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G2K.GEMFR.Website.Startup))]
+namespace G2K.GEMFR.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
